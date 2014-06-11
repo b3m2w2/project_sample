8b348806a150476763a6d43a71722f2c2251395e
U-byeongyeol_PC\byeongyeol <byeongyeol@byeongyeol_PC.(none)>
2014-06-11 13:25:39 +0900

    first commit

---
+++ BY_Sample/src/com/byeongyeol/main/SampleMainActivity.java
@@ -0,0 +1,47 @@
+package com.byeongyeol.main;
+import com.byeongyeol.sample.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SampleMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.samplemainactivity);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	
+}
